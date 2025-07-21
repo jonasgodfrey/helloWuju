f1e9f4ea4d9804439fdfd0710833fa2195c528bb
jonasgodfrey <godfreyjtech2020@gmail.com>
2025-07-21 17:02:16 -0400

    first push

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello Nathaniel! <br></br>
+          Welcome to WujuPay
+        </p>
+        <a
+          className="App-link"
+          href="https://wujupay.ca"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          You are in for a beautiful Journey
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
